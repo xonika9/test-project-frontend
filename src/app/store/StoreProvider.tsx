d3f14b2057e178ecaf6003ce3dc65a4617fcd81f
xonika9 <xonika9@gmail.com>
2025-02-21 17:31:29 +0300

    refactor: Simplify store initialization in StoreProvider

--- src/app/store/StoreProvider.tsx
+++ src/app/store/StoreProvider.tsx
@@ -2,17 +2,13 @@
 
 import { useRef } from 'react';
 import { Provider } from 'react-redux';
-import { makeStore, AppStore } from './store';
+import { AppStore, store } from './store';
 
-export default function StoreProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const storeRef = useRef<AppStore>();
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
+    const storeRef = useRef<AppStore>();
+    if (!storeRef.current) {
+        storeRef.current = store();
+    }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+    return <Provider store={storeRef.current}>{children}</Provider>;
 }
