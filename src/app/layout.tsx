de71c67f3b559873e1a6cc2f05a1779cf1cb4088
xonika9 <xonika9@gmail.com>
2025-02-21 17:49:01 +0300

    refactor: Simplify page layout and integrate MUI providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import StoreProvider from '@/app/store/StoreProvider';
+import theme from '@/app/theme';
 import './globals.css';
 
 const geistSans = Geist({
@@ -24,7 +29,16 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                <StoreProvider>
+                    <AppRouterCacheProvider>
+                        <ThemeProvider theme={theme}>
+                            <CssBaseline />
+                            {children}
+                        </ThemeProvider>
+                    </AppRouterCacheProvider>
+                </StoreProvider>
+            </body>
         </html>
     );
 }
