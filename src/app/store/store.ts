175d1b6f88c7e1f7ed24a8cf056e4f77e5e660e6
xonika9 <xonika9@gmail.com>
2025-02-21 17:21:33 +0300

    refactor: Rename `makeStore` to `store` for consistency

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 
-export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      auth: authReducer,
-    },
-  });
+export const store = () => {
+    return configureStore({
+        reducer: {
+            auth: authReducer,
+        },
+    });
 };
 
-export type AppStore = ReturnType<typeof makeStore>;
+export type AppStore = ReturnType<typeof store>;
 export type RootState = ReturnType<AppStore['getState']>;
 export type AppDispatch = AppStore['dispatch'];
