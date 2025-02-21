b81d62b08d3566d90f64359ebf60b2ddf4526aec
xonika9 <xonika9@gmail.com>
2025-02-21 17:26:37 +0300

    refactor: Replace store with makeStore in axios interceptors

--- src/utils/axiosInterceptors.ts
+++ src/utils/axiosInterceptors.ts
@@ -1,27 +1,27 @@
 import { AxiosInstance } from 'axios';
-import { store } from '@/app/store/store';
+import { makeStore } from '@/app/store/store';
 import { logout } from '@/app/store/authSlice';
 
 export const setupInterceptors = (instance: AxiosInstance) => {
-  instance.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
+    instance.interceptors.request.use(
+        config => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        },
+        error => Promise.reject(error),
+    );
 
-  instance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error.response?.status === 401) {
-        store.dispatch(logout());
-        window.location.href = '/signin';
-      }
-      return Promise.reject(error);
-    }
-  );
+    instance.interceptors.response.use(
+        response => response,
+        error => {
+            if (error.response?.status === 401) {
+                makeStore.dispatch(logout());
+                window.location.href = '/signin';
+            }
+            return Promise.reject(error);
+        },
+    );
 };
