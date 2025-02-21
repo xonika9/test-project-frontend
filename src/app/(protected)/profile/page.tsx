a8aa590269f37b9a4a92209b75f2f0211a158160
xonika9 <xonika9@gmail.com>
2025-02-21 19:07:37 +0300

    1

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,5 +1,10 @@
 import ProfilePage from '@/components/ProfilePage/ProfilePage';
+import RequireAuth from '@/components/RequireAuth/RequireAuth';
 
 export default function Profile() {
-    return <ProfilePage />;
+    return (
+        <RequireAuth>
+            <ProfilePage />
+        </RequireAuth>
+    );
 }
